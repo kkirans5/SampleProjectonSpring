dfe8d6d5924d9393a8e666fec8e47a447c74de06
kkirans5 <15h61a0483@cvsr.ac.in>
2023-02-09 12:04:34 -0500

    Adding changes to First branch

---
+++ src/main/java/com/example/sampleProject/SampleProjectonSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleProjectonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleProjectonSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sampleProject/SampleProjectonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sampleProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleProjectonSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
